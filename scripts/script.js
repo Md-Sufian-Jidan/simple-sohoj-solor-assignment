c824bf5e1536a0bd1c5a926e8e0e37ea87f246c2
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-02-18 03:34:45 +0600

    assignment setup done

---
+++ scripts/script.js
@@ -0,0 +1,40 @@
+// alert('connected');
+let count = 1;
+let left = 1;
+let sum= 0;
+
+const allSits = document.querySelectorAll('.seat-no');
+
+for(const sit of allSits){
+    // console.log(sit)
+    sit.addEventListener('click', function(e){
+        const sitTitle = document.getElementById('seat-count');
+        sitTitle.innerText = count++;
+
+        const leftSeat = document.getElementById('left-seat').innerText;
+        const convertedSeat = parseInt(leftSeat);
+        const totalSeat = convertedSeat - left;
+
+        document.getElementById('left-seat').innerText = totalSeat;
+        // const seat = e.target.innerText
+        // const title = document.getElementById('title');
+        // title.innerText = seat;
+        const disable = e.target;
+
+        disable.setAttribute('disabled', true);
+        
+        const price = document.getElementById('price').innerText;
+        const convertedPrice = parseInt(price);
+        sit.style.backgroundColor = '#1dd100';
+        // console.log(title)
+        const totalCost = document.getElementById('total-cost');
+        sum = sum + convertedPrice ;
+        totalCost.innerText = sum;
+        const grandTotal = document.getElementById('grand-total');
+        grandTotal.innerText = totalCost.innerText;
+    })
+}
+const next = document.getElementById('next');
+// function modal(elementId){
+//     const element = document.getElementById(elementId)
+// }
