c46d925e0e121f5e5d34c35a7d57f5718ec6c168
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-02-18 16:09:51 +0600

    footer section complete

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,13 @@
 // alert('connected');
 let count = 1;
 let left = 1;
-let sum= 0;
+let sum = 0;
 
 const allSits = document.querySelectorAll('.seat-no');
 
-for(const sit of allSits){
+for (const sit of allSits) {
     // console.log(sit)
-    sit.addEventListener('click', function(e){
+    sit.addEventListener('click', function (e) {
         const sitTitle = document.getElementById('seat-count');
         sitTitle.innerText = count++;
 
@@ -22,27 +22,28 @@ for(const sit of allSits){
         const disable = e.target;
 
         disable.setAttribute('disabled', true);
-        
+
         const price = document.getElementById('price').innerText;
         const convertedPrice = parseInt(price);
         sit.style.backgroundColor = '#1dd100';
+        sit.style.color = '#ffffff'
         const totalCost = document.getElementById('total-cost');
-        sum = sum + convertedPrice ;
+        sum = sum + convertedPrice;
         totalCost.innerText = sum;
         const grandTotal = document.getElementById('grand-total');
         grandTotal.innerText = totalCost.innerText;
 
         const applyBtn = document.getElementById('apply-btn');
-        applyBtn.addEventListener('click', function(){
-            
+        applyBtn.addEventListener('click', function () {
+
             const couponElement = document.getElementById('coupon-input').value;
             const couponCode = couponElement.split(' ').join('').toLowerCase();
             // console.log(couponCode);
-            
+
             const convertedTotalCost = parseFloat(totalCost.innerText);
             const sit = parseFloat(sitTitle.innerText)
-            if(sit === 4){
-                if(couponCode === 'new15'){
+            if (sit === 4) {
+                if (couponCode === 'new15') {
                     const discount = parseInt(convertedTotalCost) * .15;
 
                     const totalAmount = parseFloat(convertedTotalCost) - parseFloat(discount);
@@ -53,9 +54,21 @@ for(const sit of allSits){
                 }
             }
             alert('please buy at least four tickets to get the coupon');
+
             applyBtn.setAttribute('disabled', true);
-        })
-    })
+
+            const next = document.getElementById('next');
+            next.addEventListener('click', function () {
+                const resetLeftSeat = document.getElementById('left-seat');
+                resetLeftSeat.innerText = 40;
+
+            });
+        });
+    });
+};
+
+function scrollWin() {
+    window.scrollTo(0, 1770);
 }
 // const next = document.getElementById('next');
 // function modal(elementId){
